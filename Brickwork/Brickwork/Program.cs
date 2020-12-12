08ebfa7007174110e2de20b8c52cbe442e6acbee
Konstantin Kolev <konstantin.kolevv@gmail.com>
2020-12-12 17:53:01 +0200

    Added comments to the Program class.

--- Brickwork/Brickwork/Program.cs
+++ Brickwork/Brickwork/Program.cs
@@ -3,10 +3,19 @@ using System.Linq;
 
 namespace Brickwork
 {
+    /// <summary>
+    /// <para>The main class of the program.</para>
+    /// <para>Contains the <c>Main</c> method.</para>
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The entry point of the program.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //The input from the user for the number of rows and columns of the input.
             int[] input = Console.ReadLine()
                                  .TrimEnd()
                                  .Split(' ')
@@ -14,9 +23,18 @@ namespace Brickwork
                                  .ToArray();
             int n = input[0];
             int m = input[1];
+
+            //The first layer of bricks on which the next one will be based.
             BrickLayer firstLayer = new BrickLayer(n, m);
+
+            //Input of the first layer from the user.
             firstLayer.EnterLayout();
+
+            //Initializing of the BrickLayer object that will hold the second layer of bricks.
             BrickLayer secondLayer = new BrickLayer(firstLayer.Layout.GetLength(0), firstLayer.Layout.GetLength(1));
+
+            //Checking if there is a possible second layer of bricks and printing it if it exists.
+            //If no solution exists, an error message is displayed instead.
             if (firstLayer.GenerateNextLayer(secondLayer.Layout, 1))
             {
                 Console.WriteLine();
